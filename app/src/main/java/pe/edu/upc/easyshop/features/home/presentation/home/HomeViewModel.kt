00ee6e7d39af7ea304c1657ef11d82d5567c06ef
Far14z <faridce14@gmail.com>
2025-09-21 19:45:59 -0500

    feat: add viewmodel, product repository and manual dependency injection.

--- app/src/main/java/pe/edu/upc/easyshop/features/home/presentation/home/HomeViewModel.kt
+++ app/src/main/java/pe/edu/upc/easyshop/features/home/presentation/home/HomeViewModel.kt
@@ -1,17 +1,26 @@
 package pe.edu.upc.easyshop.features.home.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import pe.edu.upc.easyshop.features.home.domain.repositories.ProductRepository
 import pe.edu.upc.easyshop.shared.models.Product
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel(private val repository: ProductRepository): ViewModel() {
 
     private val _products = MutableStateFlow(emptyList<Product>())
 
     val products: StateFlow<List<Product>> = _products
 
     fun getAllProducts() {
+        viewModelScope.launch {
+            _products.value = repository.getAllProducts()
+        }
+    }
 
+    init {
+        getAllProducts()
     }
 }
