232b570b8b61f9df5031b2638d163f74d376789a
Far14z <faridce14@gmail.com>
2025-09-21 19:47:12 -0500

    chore: delete unused imports.

--- app/src/main/java/pe/edu/upc/easyshop/features/home/presentation/productdetail/ProductDeatil.kt
+++ app/src/main/java/pe/edu/upc/easyshop/features/home/presentation/productdetail/ProductDeatil.kt
@@ -27,11 +27,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import pe.edu.upc.easyshop.core.ui.components.RoundedICon
-import pe.edu.upc.easyshop.core.ui.theme.EasyShopTheme
 import pe.edu.upc.easyshop.shared.models.Product
 
 @Composable
